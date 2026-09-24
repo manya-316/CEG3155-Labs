b264e682771ba370be6f493bcb3e9ebbbd289881
Joshua-Deering <jidjosh2006@icloud.com>
2026-09-23 21:32:44 -0400

    L and R Registers

---
+++ vhdl/LRegister.vhd
@@ -0,0 +1,47 @@
+entity LRegister is
+    port (
+        clk : in bit;
+        shiftL : in bit;
+        load : in bit;
+        d0, d1, d2, d3, d4, d5, d6, d7 : in bit;
+        q0, q1, q2, q3, q4, q5, q6, q7 : out bit);
+end LRegister;
+
+architecture default of LRegister is
+    storage : process is 
+        variable stored_d0, stored_d1, stored_d2, stored_d3, stored_d4, stored_d5, stored_d6, stored_d7 : bit;
+    begin
+        if clk = '1' then
+            if load = '1' then
+                stored_d0 := d0;
+                stored_d1 := d1;
+                stored_d2 := d2;
+                stored_d3 := d3;
+                stored_d4 := d4;
+                stored_d5 := d5;
+                stored_d6 := d6;
+                stored_d7 := d7;
+            elsif shiftL = '1' then
+                stored_d0 := '0';
+                stored_d1 := stored_d0;
+                stored_d2 := stored_d1;
+                stored_d3 := stored_d2;
+                stored_d4 := stored_d3;
+                stored_d5 := stored_d4;
+                stored_d6 := stored_d5;
+                stored_d7 := stored_d6;
+            end if;
+        end if;
+
+        q0 <= stored_d0;
+        q1 <= stored_d1;
+        q2 <= stored_d2;
+        q3 <= stored_d3;
+        q4 <= stored_d4;
+        q5 <= stored_d5;
+        q6 <= stored_d6;
+        q7 <= stored_d7;
+
+        wait on clk, d0, d1, d2, d3, d4, d5, d6, d7, load, shiftL;
+    end process storage;
+end architecture default;
